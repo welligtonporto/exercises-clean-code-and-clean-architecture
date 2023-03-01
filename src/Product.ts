ce3be7f01643f9e4e43e4b5dca89142228d854ed
Welligton Porto <welligtonporto@gmail.com>
2023-02-28 22:41:13 -0300

    Refactoring

--- src/Product.ts
+++
@@ -1,5 +0,0 @@
-export default class Product {
-    constructor (readonly description: string, readonly price: number, readonly quantity: number) {
-        // validations (if necessary)
-    }
-}
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,31 +1,48 @@
-import Order from "../src/Order";
-import Consumer from "../src/Consumer";
+import axios from "axios";
 
-test("Deve criar um pedido com 3 produtos (com descrição, preço e quantidade) e calcular o valor total", function(){
-    const consumer = new Consumer("Welligton", "18300920064");
-    const order = new Order(consumer);
-    order.addProduct("Luva", 10, 1);
-    order.addProduct("Resina", 15, 2);
-    order.addProduct("Autoclave", 5000, 1);
-    const total = order.calculateTotal();
-    expect(total).toBe(5040);
+test("Não deve aceitar um pedido com cpf inválido", async function () {
+	const input = {
+		cpf: "406.302.170-27"
+	}
+	const response = await axios.post("http://localhost:3000/checkout", input);
+	const output = response.data;
+	expect(output.message).toBe("Invalid cpf");
 });
 
-test("Deve criar um pedido com 3 produtos, associar um cupom de desconto e calcular o total (percentual sobre o total do pedido)", function(){
-    const consumer = new Consumer("Welligton", "18300920064");
-    const order = new Order(consumer);
-    order.addProduct("Luva", 10, 1);
-    order.addProduct("Resina", 15, 2);
-    order.addProduct("Autoclave", 5000, 1);
-    order.addTicket("sunday10");
-    const total = order.calculateTotal();
-    expect(total).toBe(4536);
+test("Deve criar um pedido vazio", async function () {
+	const input = {
+		cpf: "407.302.170-27"
+	};
+	const response = await axios.post("http://localhost:3000/checkout", input);
+	const output = response.data;
+	expect(output.total).toBe(0);
 });
 
-test("Não deve criar um pedido com cpf inválido (lançar algum tipo de erro)", function(){
-    // expect(() => new Consumer("Welligton", null)).toThrow(new Error("Invalid cpf"));
-    // expect(() => new Consumer("Welligton", undefined)).toThrow(new Error("Invalid cpf"));
-    expect(() => new Consumer("Welligton", null)).toThrow(new Error("Invalid cpf"));
-    expect(() => new Consumer("Welligton", undefined)).toThrow(new Error("Invalid cpf"));
-    expect(() => new Consumer("Welligton", "11111111111")).toThrow(new Error("Invalid cpf"));
+test("Deve criar um pedido com 3 produtos", async function () {
+	const input = {
+		cpf: "407.302.170-27",
+		items: [
+			{ idProduct: 1, quantity: 1 },
+			{ idProduct: 2, quantity: 1 },
+			{ idProduct: 3, quantity: 3 }
+		]
+	};
+	const response = await axios.post("http://localhost:3000/checkout", input);
+	const output = response.data;
+	expect(output.total).toBe(6090);
+});
+
+test("Deve criar um pedido com 3 produtos com cupom de desconto", async function () {
+	const input = {
+		cpf: "407.302.170-27",
+		items: [
+			{ idProduct: 1, quantity: 1 },
+			{ idProduct: 2, quantity: 1 },
+			{ idProduct: 3, quantity: 3 }
+		],
+		coupon: "VALE20"
+	};
+	const response = await axios.post("http://localhost:3000/checkout", input);
+	const output = response.data;
+	expect(output.total).toBe(4872);
 });
